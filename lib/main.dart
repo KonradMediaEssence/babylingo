546cb83095ec4a601b63281cddb032a2d2a5b4b8
Valori-Job <adam.elcner@mediaessence.pl>
2025-06-02 15:29:32 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(94, 0, 255, 132)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
